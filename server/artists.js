c50c436af18b3eeec855cd50e2a2afb10289a173
Ellie Sterner <elliesterner13@gmail.com>
2017-04-18 16:54:02 -0500

    Artists reducer (#42)
    * added artist redux info
    
    * added artist route
    
    * added singleArtist view
    
    * added singleArtist view

---
+++ server/artists.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const db = require('APP/db')
+const User = db.model('users')
+const Order = db.model('orders')
+const ProductDetail = db.model('productDetails')
+const Product = db.model('products')
+const Comment = db.model('comments')
+
+const {mustBeLoggedIn, forbidden} = require('./auth.filters')
+
+module.exports = require('express').Router()
+  .get('/',
+    (req, res, next) =>
+      User.findAll({
+        where: {isArtist: true},
+        include: [{model: Order,
+          include: [{model: ProductDetail,
+            include: [{model: Product}]
+          }]
+        }, {model: Comment}]
+      })
+        .then(users => res.json(users))
+        .catch(next))
+  .post('/',
+    (req, res, next) =>
+      User.create(req.body)
+      .then(user => res.status(201).json(user))
+      .catch(next))
+  .get('/:id',
+    //mustBeLoggedIn,
+    (req, res, next) =>
+      User.findById(req.params.id)
+      .then(user => res.json(user))
+      .catch(next))
