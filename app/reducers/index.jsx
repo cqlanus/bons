9dbfd737e58d2c68a0d94337b7d6d027bb20b096
Ellie Sterner <elliesterner13@gmail.com>
2017-04-18 11:59:19 -0500

    Redux store users (#31)
    * added redux for user
    
    * edited store

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -3,6 +3,9 @@ import { combineReducers } from 'redux'
 const rootReducer = combineReducers({
   auth: require('./auth').default,
   products: require('./products').default,
+  orders: require('./orders').default,
+  users: require('./user').default
+
 })
 
 export default rootReducer
