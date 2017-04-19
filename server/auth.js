42030035d8366546ea84bc1e5a830b6f503130ad
Chris Lanus <cqlanus@gmail.com>
2017-04-19 10:16:59 -0500

    working on login

--- server/auth.js
+++ server/auth.js
@@ -117,7 +117,10 @@ passport.use(new (require('passport-local').Strategy)(
   }
 ))
 
-auth.get('/whoami', (req, res) => res.send(req.user))
+auth.get('/whoami', (req, res) => {
+  console.log(req.user)
+  res.send(req.user)
+})
 
 // POST requests for local login:
 auth.post('/login/local', passport.authenticate('local', {successRedirect: '/'}))
