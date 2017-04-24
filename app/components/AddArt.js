07b97fa872e8871d25dfd58c2afa63a6d20b1eef
Alyssa Ramsey <aly.m.ramsey@gmail.com>
2017-04-24 14:08:22 -0500

    Added orders (#66)
    * starting productslist page
    
    * other commit
    
    * another commit
    
    * products list and panel pages set up
    
    * products list and panel pages set up
    
    * started working on orders pages
    
    * orders pages almost done
    
    * finished order panel
    
    * order list page and panel finished
    
    * putting together order put route
    
    * another commit
    
    * last commit before switching to styleFun branch
    
    * orders stuff done
    
    * snwfouw
    
    * addProduct thunk created
    
    * post art route finished minus the img stuff
    
    * ok REALLY finished the add art minus the img

--- app/components/AddArt.js
+++ app/components/AddArt.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { postProduct } from '../reducers/products'
 
 
 const mapStateToProps = (state) => ({
@@ -7,7 +8,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
-  // putPayment: putPayment
+    postProduct: postProduct,
 }
 
 
@@ -17,52 +18,69 @@ class AddArt extends React.Component {
   constructor() {
     super()
     this.state = {
-      title: '',
+      name: '',
       description: '',
       unitPrice: 0.01,
-      category: '',
+      categories: [],
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.checkHandleChange = this.checkHandleChange.bind(this)
+  }
+
+  checkHandleChange(evt){
+    var categoryId = evt.target.value
+    var currentCategories = this.state.categories
+    var index = currentCategories.indexOf(categoryId)
+    if(index === -1){
+      var newStateCategories = [...currentCategories, categoryId]
+    } else {
+      var newStateCategories = currentCategories.splice(index, 1)
+    }
+    this.setState({
+      categories: newStateCategories,
+    })
+    // console.log("UPDATED LOCAL STATE IS", this.state)
   }
 
   handleChange(evt) {
-    console.log(evt.target.value)
     var type = evt.target.name
     var value = evt.target.value
     this.setState({
       [type]: value,
     })
+    // console.log("UPDATED LOCAL STATE IS", this.state)
   }
 
   handleSubmit(evt){
     evt.preventDefault()
-
-
+    console.log("THIS.PROPS", this.props)
+    this.props.postProduct(this.state)
   }
 
   render() {
     return (
       <div>
-      <form className="form-horizontal">
+      <form className="form-horizontal" onSubmit={this.handleSubmit}>
       <fieldset>
         <legend>Add Art</legend>
         <div className="form-group">
           <label>Title</label>
-          <input name="title" type="text" className="form-control" />
+          <input name="name" type="text" className="form-control" onChange={this.handleChange}/>
         </div>
 
         <div className="form-group">
           <label>Description</label>
-          <input name="description" type="text" className="form-control" />
+          <input name="description" type="text" className="form-control" onChange={this.handleChange}/>
         </div>
 
         <div className="form-group">
           <label>Cost</label>
           <div className="input-group">
             <span className="input-group-addon">$</span>
-            <input name="unitPrice" type="number" step="0.01" className="form-control" />
+            <input name="unitPrice" type="number" min="0" step="0.01" className="form-control" onChange={this.handleChange}/>
           </div>
         </div>
 
@@ -71,12 +89,47 @@ class AddArt extends React.Component {
           <input name="category" type="text" className="form-control" />
         </div> */}
 
+        <div className="form-group">
+          <label>Select all categories that apply:</label>
+        </div>
+        <div>
+          <input type="checkbox" name="categories" value="1" onChange={this.checkHandleChange}/>
+          <label>Drawing</label>
+        </div>
+        <div>
+          <input type="checkbox" name="categories" value="2" onChange={this.checkHandleChange}/>
+          <label>Painting</label>
+        </div>
+        <div>
+          <input type="checkbox" name="categories" value="3" onChange={this.checkHandleChange}/>
+          <label>Digital</label>
+        </div>
+        <div>
+          <input type="checkbox" name="categories" value="4" onChange={this.checkHandleChange}/>
+          <label>Jewlery</label>
+        </div>
+        <div>
+          <input type="checkbox" name="categories" value="5" onChange={this.checkHandleChange}/>
+          <label>Home Decor</label>
+        </div>
+        <div>
+          <input type="checkbox" name="categories" value="6" onChange={this.checkHandleChange}/>
+          <label>Photograph</label>
+        </div>
+        <div>
+          <input type="checkbox" name="categories" value="7" onChange={this.checkHandleChange}/>
+          <label>Mixed Media</label>
+        </div>
+
 
 
 
         <div className="form-group">
           <label>Upload</label>
           <input type="file" name="img" accept="image/*" onChange={this.handleChange}/>
+        </div>
+        <div>
+          <button type="submit" className="btn btn-danger pull-right">Submit</button>
         </div>
       </fieldset>
       </form>
@@ -85,6 +138,6 @@ class AddArt extends React.Component {
   }
 }
 
-const AddArtContainer = connect(null, null)(AddArt)
+const AddArtContainer = connect(mapStateToProps, mapDispatchToProps)(AddArt)
 
 export default AddArtContainer
