557cd3521f3471254312e47cefb25112fb16d0fb
elliesterner13@gmail.com <elliesterner@Ellies-MacBook-Pro.local>
2017-04-24 15:27:49 -0500

    select payment profile dynamic

--- app/components/PaymentForm.js
+++ app/components/PaymentForm.js
@@ -16,14 +16,12 @@ export class PaymentForm extends React.Component {
   constructor() {
     super()
     this.state = {
-      profile: {},
+      profile: 0,
       type: '',
       typeOfCard: '',
       name: '',
       creditnumber: '',
       expiration: '',
-      // month: '',
-      // year: '',
       securitycode: '',
       zip: '',
     }
@@ -31,6 +29,7 @@ export class PaymentForm extends React.Component {
     // this.handleExpChange = this.handleExpChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     // this.smooshExpDate = this.smooshExpDate.bind(this)
+    this.handleProfileChange = this.handleProfileChange.bind(this)
   }
 
   handleChange(evt) {
@@ -41,6 +40,17 @@ export class PaymentForm extends React.Component {
     })
     // console.log(this.state)
   }
+
+  handleProfileChange(evt) {
+    console.log('in handleProfile Change')
+    console.log('this', this)
+    const type = evt.target.name
+    const value = evt.target.value
+    this.setState({
+      [type]: value
+    })
+  }
+    
 
 
   handleSubmit(evt) {
@@ -63,9 +73,11 @@ export class PaymentForm extends React.Component {
                 <label htmlFor="type">Select payment profile:</label>
               </div>
               <div>
-                <select name="type" className="form-control col-xs-2" onChange={this.handleChange}>
-                  <option>Card</option>
-                  <option>Cash</option>
+                <select name="profile" className="form-control col-xs-2" onChange={this.handleProfileChange}>
+                  <option>Enter new payment information</option>
+                  {this.props.payment.payment_profiles.map((paypro=>{
+                    return (<option key={paypro.id} value={parseInt(paypro.id)}> {paypro.name+' '+ '('+paypro.creditnumber.slice(-4)+')'} </option>)
+                  }))}}
                 </select>
               </div>
             </div>
